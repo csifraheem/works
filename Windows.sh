2d6503200cb5720aca248726a96838a12d32725b
csifraheem <104068893+csifraheem@users.noreply.github.com>
2022-04-30 19:41:46 +0500

    Add files via upload

---
+++ Windows.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# set the varGreeting variable
+varGreeting="Hello World!"
+# print the contents of the variable varGreeting on screen
+echo $varGreeting
+echo "Hello I have created it using Windows Operating System."
